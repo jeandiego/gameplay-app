dc515036a6aa3e73ced61aff303bb684da3bd267
jeeanjames <jeandiego.ds@gmail.com>
2021-07-16 16:08:32 -0300

    🎨 Updated screens structures

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
-import {
-  View, 
-  Text, 
-  Image, 
-  StatusBar } from 'react-native';
-import { styles } from './styles';
-import IllustrationImg from '../../assets/illustration.png';
-import { ButtonIcon } from '../../components/ButtonIcon';
+import  SignInView  from './view';
 
 
-
-export function SignIn(){
+const SignIn = () => {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      <Image source={IllustrationImg} style={styles.image} resizeMode="stretch" />
-
-      <View style={styles.content}>
-        <Text style={styles.title}> Conecte-se {`\n`} e organize suas {`\n`} jogatinas</Text>
-        <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`} favoritos com seus amigos
-        </Text>
-        <ButtonIcon 
-          title="Entrar com Discord" 
-          activeOpacity={0.6}/>
-      </View>
-    </View>
-  );
+    <SignInView />
+  )
 }
+
+export default SignIn
